379b1d7535229643818c1e6d3ebe3ad441426235
Oscar <ocastillo.cl@gmail.com>
2026-08-04 14:36:41 -0400

    Avances semana 05

---
+++ semana-05/dia03_ejercicio01.js
@@ -0,0 +1,25 @@
+let equipoA = ["Ana", "Pedro", "María"];
+let equipoB = ["Juan", "Sofía", "Carlos"];
+let nuevosIntegrantes = ["Lucía", "Diego"];
+
+// 1. Crea un array `equipoCompleto` que combine ambos equipos
+
+let equipoCompleto = [...equipoA, ...equipoB];
+
+// 2. Crea un array `equipoAmpliado` con `equipoA` + los nuevos integrantes al final
+
+let equipoAmpliado = [...equipoA, ...nuevosIntegrantes];
+
+// 3. Crea una copia de `equipoA` y agrega "Roberto" sin modificar el original
+
+let copiaEquipoA = [...equipoA, "Roberto"];
+
+
+
+// 4. Encuentra el nombre más largo de `equipoCompleto` usando spread con `Math.max` y `.length`
+
+let nombreMasLargo = equipoCompleto.map((nombres) => {
+    nombres.length;
+});
+ 
+console.log(Math.max(...nombreMasLargo));

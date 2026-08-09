5b4a4c80b8f4ab89316b8e30ba44f14049de7bf4
Oscar <ocastillo.cl@gmail.com>
2026-08-09 00:17:26 -0400

    Avances semana 05

--- semana-05/dia04_ejercicio02.js
+++ semana-05/dia04_ejercicio02.js
@@ -10,3 +10,19 @@ let formatearDinero = ({ monto, moneda = "CLP", decimales = 0 }) =>
 
 let resultado = formatearDinero({ monto: 450000, moneda: "USD", decimales: 2 });
 console.log(resultado);
+
+//**2. `filtrarExpediciones(expediciones, { tipo, dificultad, precioMax = Infinity })`**
+
+const expediciones = [
+  { nombre: "Cruce Los Andes", tipo: "trekking", dificultad: "alta", precioBase: 280000 },
+  { nombre: "Lago Llanquihue", tipo: "kayak", dificultad: "media", precioBase: 195000 },
+  { nombre: "Reserva Nonguén", tipo: "trekking", dificultad: "baja", precioBase: 45000 }
+];
+
+
+let filtrarExpediciones = (expediciones, { tipo, dificultad, precioMax = Infinity }) => {return expediciones.filter((exp) => (!tipo || exp.tipo === tipo) &&
+    (!dificultad || exp.dificultad === dificultad) &&
+    exp.precioBase <= precioMax);
+  }
+
+  filtrarExpediciones(expediciones, {});

321f55b9456dd638ba0dcf3d758b6e244f08dad4
Oscar <ocastillo.cl@gmail.com>
2026-08-17 01:19:18 -0400

    Avances semana 6, ejercicios dia 1

---
+++ semana-06/dia01_ejercicio02.js
@@ -0,0 +1,4 @@
+console.log("Iniciando sistema...");
+setTimeout(() => console.log("Cargando expediciones..."), 500);
+setTimeout(() => console.log("Cargando usuarios..."), 1000);
+setTimeout(() => console.log("Sistema listo"), 1500);
